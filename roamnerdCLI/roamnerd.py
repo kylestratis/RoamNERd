69f4029c1a63f7cf4f1f3415fba37d343c408f6f
hmprt <hmprt@protonmail.com>
2020-09-25 21:38:35 +0100

    Added CLI implementation, batch processing

--- roamnerdCLI/roamnerd.py
+++ roamnerdCLI/roamnerd.py
@@ -1,10 +1,17 @@
 import argh
-import pdb
-import os
+import os.path
 import requests
 
 ## V0.1 of roamnerd CLI - supports only .txt input.
-def main(verbose=False, all=False, *files):
+@argh.arg("files", help="File(s) to load.")
+def main(verbose : "Display extra information about [[tags]] (not yet implemented)" =False,
+            all : "Concatenate all results into one .md file with shared [[tags]] (not yet implemented)"=False,
+            *files):
+
+
+    for file in files:
+        assert file.endswith(".txt"), "One or more of your files is not a .txt file"
+        assert os.path.isfile(file), "One or more of your files does not exist or is in a different directory"
 
     url = "https://roamnerd-be.herokuapp.com/tagText"
 
@@ -15,6 +22,7 @@ def main(verbose=False, all=False, *files):
 
         with open(file[:-4] + ".md", "w") as out:
             out.write(x.text)
+            print("Out: " + file[:-4] + ".md")
 
 
 argh.dispatch_command(main)
